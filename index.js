3af5f7d1ba20b98f993412876d3323d16f8e963a
PAWAN S <93472805+spawan02@users.noreply.github.com>
2023-06-02 00:04:50 +0530

    Add files via upload
    npm packages used are inquirer, qr-image.

---
+++ index.js
@@ -0,0 +1,21 @@
+
+import inquirer from "inquirer";
+import qr from 'qr-image';
+import fs from "file-system";
+inquirer
+  .prompt([
+    {
+      message: "Type in your URL: ",
+      name: "URL",
+    },
+  ])
+  .then((answers) => {
+    const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream("qr_ig.png"));
+
+    fs.writeFile("URL.txt", url, (err) => {
+      if (err) throw err;
+      console.log("The file has been saved!");
+    });
+  });
